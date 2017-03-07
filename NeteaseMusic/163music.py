588d137b8f47bcba824256248a3578a9c7eb89a5
yan.yf <yan.yf@aliyun.com>
2017-03-07 17:42:37 +0800

    add parsecomments func

--- NeteaseMusic/163music.py
+++ NeteaseMusic/163music.py
@@ -1,11 +1,12 @@
 import requests
+import re
+import json
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import random
 
 
 class Music163(object):
-    # headers['User-Agent'] = UA[random.randint(0, len(UA) - 1)]
     def __init__(self):
         self.name = 'music163'
         self.UA = [
@@ -21,17 +22,40 @@ class Music163(object):
             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)'
         ]
         self.headers = {
+            'Cookie': 'appver=1.5.0.75771',
             'Referer': 'http://music.163.com/',
             'Host': 'music.163.com',
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        }
+        self.form_data = {
+            'params': 'MLdHd98OgCy6FyF1iHjKEaNx+vcI8AoHgys1NasyRuaooT/AoEByjdWFml+vUbrXUKW2PUhaTHwnsK56ZDl8kh1zoB92IGUZJPQZQ86j8XoUg0aTcdg426Odd8uUWzPa',
+            'encSecKey': 'ddb05389b2484153822e55859d797146e4041cffb5076f8076e053b659a081e5e03ef7a74479ce669ff1709b09dae0fb84ebdc6050f13a761d0711f5c2f235cd6dba31a8d6003780baa49ae1574fe83ec5804b61c8e43ac5bc6205feee85ae4508fe68780e80fec7d41a1facf154b1d5bb9b91b010cd69a5728de0db47935a75'
         }
         self.artists = []
         '''/artist?id=6452'''
         '''http://music.163.com/#/artist?id=6452'''
 
     def get_top_comments(self, url):
-        pass
+        song_id = re.findall('=\d+', url)
+        print(song_id)
+        song_id = song_id[0][1:] 
+        url = 'http://music.163.com/weapi/v1/resource/comments/R_SO_4_%s/?csrf_token=' % (song_id,)
+        try:
+            r = requests.post(url, headers=self.headers, data=self.form_data)
+            try:
+                hot_comments_js = json.loads(r.text)['hotComments']
+                total_comments = json.loads(r.text)['total']
+                hot_comments = ''
+                for item in hot_comments_js:
+                    hot_comments = hot_comments + item['user']['nickname'] + ':' + item['content'] + '\n'
+                with open('info.txt', 'a+') as f:
+                    f.write(str(total_comments))
+                    f.write(hot_comments.encode('gbk', 'ignore').decode('gbk').rstrip())
+            except:
+                print('RequestsError\n')
+        except:
+            print('Requests' + song_id)
 
     def get_songs_url(self):
         url = 'http://music.163.com'
@@ -39,18 +63,21 @@ class Music163(object):
             art_url = url + item
             self.headers['User-Agent'] = self.UA[random.randint(0, len(self.UA) - 1)]
             s = requests.session()
-            s.keep_alive = False
-            soup = BeautifulSoup(s.get(art_url, headers=self.headers).content, 'lxml')
-            song_list = soup.find('ul', {'class': 'f-hide'})
-            for song in song_list.find_all('a'):
-                song_url = urljoin(art_url, str(song['href']))
-                self.get_top_comments(song_url)
+            try:
+                soup = BeautifulSoup(s.get(art_url, headers=self.headers).content, 'lxml')
+                song_list = soup.find('ul', {'class': 'f-hide'})
+                for song in song_list.find_all('a'):
+                    song_url = urljoin(art_url, str(song['href']))
+                    with open('info.txt', 'a+') as f:
+                        f.write(song.text.encode('gbk', 'ignore').decode('gbk').rstrip() + '\n')
+                    self.get_top_comments(song_url)
+            except:
+                print('Max retries exceeded with url' + item)
 
     def get_all_artist(self, url):
         print(url)
         self.headers['User-Agent'] = self.UA[random.randint(0, len(self.UA) - 1)]
         s = requests.session()
-        s.keep_alive = False
         soup = BeautifulSoup(s.get(url, headers=self.headers).content, 'lxml')
         main = soup.find('ul', {'class': 'm-cvrlst m-cvrlst-5 f-cb'})
         for artist in main.find_all('a', {'class': 'nm nm-icn f-thide s-fc0'}):
@@ -59,10 +86,9 @@ class Music163(object):
 
     # 爬取歌手主页
     def start(self):
-        ids = ['1001', '1002', '1003', '2001', '2002', '2003',
-               '4001', '4002', '4003', '6001', '6002', '6003',
-               '7001', '7002', '7003']
-        # ids = ['1001']
+         ids = ['1001', '1002', '1003', '2001', '2002', '2003',
+                '4001', '4002', '4003', '6001', '6002', '6003',
+                '7001', '7002', '7003']
         urls = []
         for id in ids:
             urls.append(f'http://music.163.com/discover/artist/cat?id={id}')
@@ -70,7 +96,6 @@ class Music163(object):
             print(url)
             self.headers['User-Agent'] = self.UA[random.randint(0, len(self.UA) - 1)]
             s = requests.session()
-            s.keep_alive = False
             soup = BeautifulSoup(s.get(url, headers=self.headers).content, 'lxml')
             az = soup.find('ul', {'class': 'n-ltlst f-cb'})
             for item in az.find_all('a'):
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Spiders
 Some spider programs <br/>
-NeteaseMusic为爬取网易云音乐所有歌手热门歌曲的精彩评论，目前解析到歌曲网页，卡在获得精彩评论步骤
+NeteaseMusic为爬取网易云音乐所有歌手热门歌曲的精彩评论，info.txt为解析的部分内容，后续计划添加mongodb模块存储信息

e94f16962405aa5e5d4d4fd2df4fd485a8b619a0
yan.yf <yan.yf@aliyun.com>
2017-03-08 21:39:39 +0800

    add mongodb func

--- NeteaseMusic/mongotest.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client['Netease']
-table = db['netsongs']
-d = {'artist':'chou','song':'gaobaiqiqiu','CommentsNum':'123456','url':'http:132.com'}
-table.insert(d)
